66333c6a33d259ef16e3f59b27a5604e015ae9c7
AbdAllahBoda <AbdAllah.Boda13@gmail.com>
2015-12-02 17:55:37 +0200

    Create includes.h
    headers all here.

---
+++ headers/includes.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <algorithm>
+#include <omp.h>
+#include <iostream>
+#include <math.h>
+#include <ctime>
+#include <vector>
+#include <limits.h>
+#include <conio.h>
+#include <amp.h>
+using namespace std;
+using namespace Concurrency;
